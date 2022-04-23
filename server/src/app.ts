2f168168d307dd392025d8c7936dc4e8507b2823
einisto <79069176+einisto@users.noreply.github.com>
2022-04-23 20:07:25 +0300

    Basic logging with Winston & Morgan

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,9 @@
 import express from "express"
 import postsRouter from "./routes/posts"
 import indexRouter from "./routes/index"
-import "dotenv/config"
 import { apiGetErrorHandler } from "./middlewares/apiErrorHandler"
+import morganLogger from "./middlewares/morganLogger"
+import "dotenv/config"
 
 const app = express()
 const port = process.env.PORT
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use("/", indexRouter)
 app.use("/posts", postsRouter)
 
+app.use(morganLogger)
 app.use(apiGetErrorHandler)
 
 app.listen(port, () => {
--- server/src/configs/knexfile.ts
+++ server/src/configs/knexfile.ts
@@ -18,6 +18,22 @@ const config: { [key: string]: Knex.Config } = {
       tableName: "knex_migrations",
       directory: "migrations"
     }
+  },
+  production: {
+    client: "pg",
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "migrations"
+    }
   }
 }
 
