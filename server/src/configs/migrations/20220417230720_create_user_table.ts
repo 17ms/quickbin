b4ea9501e19be3ada7ff9c8afec107eb1a76639b
einisto <79069176+ubery@users.noreply.github.com>
2022-04-28 13:45:16 +0300

    Removed UUID as unnecessary

--- server/src/configs/migrations/20220417230720_create_user_table.ts
+++ server/src/configs/migrations/20220417230720_create_user_table.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex"
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("user", (table: Knex.TableBuilder) => {
-    table.uuid("id").primary().notNullable().unique()
+    table.string("id").primary().notNullable().unique()
     table.string("nickname").notNullable()
     table.timestamps(true, true)
   })
--- server/src/configs/migrations/20220417230739_create_post_table.ts
+++ server/src/configs/migrations/20220417230739_create_post_table.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex"
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("post", (table: Knex.TableBuilder) => {
-    table.uuid("id").primary().notNullable().unique()
+    table.string("id").primary().notNullable().unique()
     table.timestamps(true, true)
     table.text("title").nullable()
     table.text("content").notNullable()
