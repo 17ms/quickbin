2ace3b378f16e256c10ee60e63d636bdc811eb21
einisto <79069176+ubery@users.noreply.github.com>
2022-05-23 23:32:40 +0300

    Read .env properly when initially running migrations

--- src/utils/parseEnv.ts
+++ src/utils/parseEnv.ts
@@ -1,4 +1,17 @@
 import dotenv from "dotenv"
 import path from "path"
 
-dotenv.config({ path: path.join(__dirname, "../../..", ".env") })
+/* Additional check for running migrations,
+ as the working directory changes from dist/ to src/configs/ */
+
+const workDir = process.cwd().split("/")
+
+if (
+  workDir[workDir.length - 1].toLowerCase() === "configs" &&
+  workDir[workDir.length - 2].toLowerCase() === "src"
+) {
+  dotenv.config({ path: path.join(__dirname, "../..", ".env") })
+  console.log("Migrations")
+} else {
+  dotenv.config({ path: path.join(__dirname, "../../..", ".env") })
+}
